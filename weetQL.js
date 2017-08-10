f2244d6ed92f676fabd82a0525d219aff9e33646
rolldone <donny.rollproject@gmail.com>
2017-08-10 11:09:13 +0800

    Add files via upload

---
+++ weetQL.js
@@ -0,0 +1,114 @@
+define(function(require){
+	let gg = function(){
+		let theJSON = function(){
+			// ALL
+			this.model = '';
+			this.field = [];
+			this.fieldCheck = {};
+			this.backendField = [];
+			this.backendFilter = [];
+			this.validator = [];
+			this.overrideProcess = null;
+			this.where = [];
+			this.column = [];
+			this.mode = 'single';
+			// laravel
+			this.with = [];
+			this.init = function(){
+				// ALL
+				this.model = '';
+				this.field = [];
+				this.fieldCheck = {};
+				this.where = [];
+				this.backendField = [];
+				this.column = [];
+				this.backendFilter = [];
+				this.overrideProcess = null;
+				this.validator = [];
+				this.mode = 'single';
+				// laravel
+				this.with = [];
+			}
+			this.init();
+		}
+		this.newJSON = new theJSON();
+		this.queryStore = [];
+		this.init = function(model,mode){
+			this.newJSON = new theJSON();
+			vm.newJSON.model = model;
+			vm.newJSON.mode = mode;
+		}
+		// untuk define field dari server
+		this.setBackendField = function(nameField,value,func){
+			vm.newJSON.backendField.push({
+				field : nameField,
+				value : value,
+				func : func
+			});
+			vm.newJSON.fieldCheck[nameField] = value;
+		}
+		this.setField = function(nameField,value){
+			vm.newJSON.field.push({
+				field : nameField,
+				value : value
+			})
+			vm.newJSON.fieldCheck[nameField] = value;
+		}
+		// becareful use this
+		// your all function gonna insert all
+		this.setFieldObject = function(theObject){
+			vm.newJSON.field = [];
+			for(var key in theObject){
+				// console.log(key,theObject[key]);
+				vm.newJSON.field.push({
+					field : key,
+					value : theObject[key]
+				});
+			}
+			// vm.newJSON.field.push(theObject);
+		}
+		this.where = function(field,operator,value){
+			vm.newJSON.where.push({
+				field : field,
+				operator : operator,
+				value : value
+			})
+		}
+		this.setColumn = function(column){
+			vm.newJSON.column.push(column);
+		}
+		this.setValidator = function(func){
+			vm.newJSON.validator.push(func);
+		}
+		this.setOverrideProcess = function(func){
+			vm.newJSON.overrideProcess = func;
+		}
+		this.setBackendFilter = function(func){
+			vm.newJSON.backendFilter.push(func);
+		}
+		this.get = function(){
+			vm.newJSON.action = 'get'; 
+			vm.queryStore.push(Object.assign({},vm.newJSON));
+		}
+		this.saveReturn = function(){
+			vm.newJSON.action = 'saveReturn'; 
+			let gg = Object.assign({},vm.newJSON);
+			vm.queryStore.push(Object.assign({},gg));
+		}
+		this.save = function(){
+			vm.newJSON.action = 'save'; 
+			let gg = Object.assign({},vm.newJSON);
+			vm.queryStore.push(Object.assign({},gg));
+		}
+		this.delete = function(){
+			vm.newJSON.action = 'delete';
+			vm.queryStore.push(Object.assign({},vm.newJSON));
+		}	
+		this.export = function(){
+			return JSON.stringify(vm.queryStore);
+		}
+		let vm = this;
+		return vm;
+	}
+	return gg;
+})
---
+++ weetQLController.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Classes\weetQL;
+
+use Illuminate\Http\Request;
+
+trait weetQLController
+{
+    public $requireFilter = null;
+	public function get(Request $request){
+
+    }
+    public function put(Request $request){
+
+    }
+    public function getRequireFilter($aa,$theFilter){
+        $requireFilter = new RequireFilter($aa,$theFilter);
+        return $requireFilter;
+    }
+    public function any(Request $request){
+    	$ww = json_decode($request->restQl);
+        $validator_valid = null;
+        $isOverrideOpen = false;
+        for($a=0;$a<count($ww);$a++){
+            $gg = $ww[$a];
+            $oo = $gg->model;
+            $class = 'App\\'.$oo;
+            $class = new $class();
+            if(isset($gg->backendFilter[0])){
+            	for($aeo=0;$aeo<count($gg->backendFilter);$aeo++){
+            		$class = $this->{$gg->backendFilter[$aeo]}($class);
+            	}
+            }
+            if(isset($gg->with[0])){
+            	for($ioi=0;$ioi<count($gg->with);$ioi++){
+            		$class = $class->with($gg->with[$ioi]);
+            	}
+            }
+            // jika menggunakan backend filter maka where tidak di perbolehkan
+            // prioritas di backendFilter
+            if(isset($gg->backendFilter[0]) == false){
+                if(isset($gg->where[0])){
+                	foreach ($gg->where as $key => $value) {
+                         // echo ($value->field);
+                         $class = $class->where($value->field,$value->operator,$value->value);
+                    }
+                }
+            }else{
+                return response()->json([
+                    'status' => 'rejected',
+                    'message' => 'You cant use backendFilter and where Clause together'
+                ],500);
+            }
+            if(isset($gg->validator[0])){
+                for($aio=0;$aio<count($gg->validator);$aio++){
+                    $validator_valid = $this->{$gg->validator[$aio]}($class,$gg,$request);
+                    if($validator_valid != null){
+                        break;
+                    }
+                }
+            }
+            if($validator_valid != null){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator_valid
+                ],500);
+            }
+            // ini untuk override process default jadi full custom code di function
+            // var_dump($gg->overrideProcess);
+            if(isset($gg->overrideProcess)){
+                return $this->{$gg->overrideProcess}($class,$gg,$request);
+            }
+            // get model class
+            switch($gg->mode){
+                case 'single':
+                    if(isset($gg->column[0])){
+                        $class = $class->first($gg->column);   
+                    }else{
+                        $class = $class->first(); 
+                    }
+                break;
+                case 'all':
+                    if(isset($gg->column[0])){
+                        $class = $class->get($gg->column);
+                    }else{
+                        $class = $class->get();
+                    }
+                break;
+            }
+            if(isset($gg->field)){
+                foreach ($gg->field as $key => $value) {
+                     // echo ($value->field);
+                     $class->{$value->field} = $value->value;
+                }
+            }
+            if(isset($gg->backendField)){
+                foreach ($gg->backendField as $key => $value) {
+                    $class->{$value->field} = $this->{$value->func}($value->value);
+                }
+            }
+            switch($gg->action){
+            	case 'save':
+                /*
+                    if(isset($class->saveFirewall[0])){
+                        $saveValid = false;
+                        for($bio=0;$bio<count($class->saveFirewall);$bio++){
+                            for($qqr=0;$qqr<count($gg->backendFilter);$qqr++){
+                                if($class->saveFirewall[$bio] == $gg->backendFilter[$qqr]){
+                                    $saveValid = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if($saveValid){
+                            $class->save();
+                        }else{
+                            return response()->json([
+                                'status' => 'error',
+                                'message' => 'You are not have Privilge saving!'
+                            ],500);
+                        }
+                    }else{
+                        $class->save();
+                    }
+                    */
+            	break;
+            	case 'delete':
+            		$class->delete();
+            	break;
+            	case 'saveReturn':
+                /*
+                    if(isset($class->saveFirewall[0])){
+                        $saveValid = false;
+                        for($bio=0;$bio<count($class->saveFirewall);$bio++){
+                            for($qqr=0;$qqr<count($gg->backendFilter);$qqr++){
+                                if($class->saveFirewall[$bio] == $gg->backendFilter[$qqr]){
+                                    $saveValid = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if($saveValid){
+                            $class->save();
+                            return response()->json([
+                                'status' => 'success',
+                                'message' => $class
+                            ],201);
+                        }else{
+                            return response()->json([
+                                'status' => 'error',
+                                'message' => 'You are not have Privilge saving!'
+                            ],500);
+                        }
+                    }else{
+                        $class->save();
+                        return response()->json([
+                            'status' => 'success',
+                            'message' => $class
+                        ],201);
+                    }
+                    */
+            	break;
+            	case 'get':
+                    try{
+                        return response()->json([
+                            'status' => 'success',
+                            'message' => $class
+                        ],201);
+                    }catch(\Exception $ex){
+                        return response()->json([
+                            'status' => 'error',
+                            'message' => $ex->getMessage()
+                        ],500);
+                    }
+            		
+            	break;
+            }
+
+            // $class->{$gg->action}();
+
+        }
+    }
+}
+
+class RequireFilter{
+    private $storeArrayFilter = [];
+    private $isReadyBackendFilter = true;
+    private $isEquals = false;
+    private $saveRequireFilter = '';
+    function __construct($gg,$arrayFunction){
+        $this->storeArrayFilter = $arrayFunction;
+        if(isset($gg->backendFilter[0])){
+            for($aa=0;$aa<count($gg->backendFilter);$aa++){
+                for($bb=0;$bb<count($arrayFunction);$bb++){
+                    if($arrayFunction[$bb] == $gg->backendFilter[$aa]){
+                        $this->isEquals = true;
+                        break;
+                    }else{
+                        $this->saveRequireFilter = $arrayFunction[$bb];
+                    }
+                }     
+                if($this->isEquals == false){
+                    break;
+                }
+            } 
+        }else{
+            $this->isReadyBackendFilter = false;
+        }
+    }
+    
+    public function rejected(){
+        if($this->isEquals == false) return true;
+    }
+    
+    public function message(){
+        if($this->isReadyBackendFilter){
+            if($this->isEquals == false){
+                return [
+                    'status'=>'rejected',
+                    'message'=>'need filter '.$this->saveRequireFilter
+                ];
+            } 
+        }else{
+            return [
+                'status'=>'rejected',
+                'message'=>'need filter '.implode(",",$this->storeArrayFilter)
+            ];  
+        }
+    }
+}
+
